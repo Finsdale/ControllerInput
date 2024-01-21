d1d1bf602b0caea14877b893ed5a413e6d2180f9
Finsdale <Talif_Moor@yahoo.com>
2024-01-21 12:42:51 -0500

    starting a rework

--- ControllerInput/InputButton.cs
+++ ControllerInput/InputButton.cs
@@ -6,20 +6,27 @@ using System.Threading.Tasks;
 
 namespace ControllerInput
 {
-    public class InputButton
+  public class InputButton
+  {
+    public bool Pressed { get; set; }
+    public bool Held { get; set; }
+    public bool Released { get; set; }
+
+    public InputButton()
     {
-        public bool Pressed { get; set; }
-        public bool Held { get; set; }
-
-        public InputButton()
-        {
-            Pressed = false;
-            Held = false;
-        }
-
-        public override string ToString()
-        {
-            return (Pressed || Held) ? "true" : "false";
-        }
+      Pressed = false;
+      Held = false;
+      Released = false;
     }
+
+    public override string ToString()
+    {
+      return (Pressed || Held) ? "true" : "false";
+    }
+  }
+
+  static public class NewInputButton
+  {
+
+  } 
 }
---
+++ ControllerInput/NewInput.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControllerInput
+{
+  internal class NewInput
+  {
+    GamePadState Cont1, Cont2, Cont3, Cont4;
+    KeyboardState KbState;
+    MouseState MState;
+  }
+}
